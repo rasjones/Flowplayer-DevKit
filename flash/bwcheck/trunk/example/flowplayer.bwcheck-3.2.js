4a0c199f912e8e1ed4d43641b586e02af3c58e89
anssip <anssip@00d34140-b561-11dd-8610-bdbd52298f7b>
2010-04-11 11:19:16 +0000

    moved flowplayer.bwcheck.js to example
    git-svn-id: http://flowplayer-plugins.googlecode.com/svn@956 00d34140-b561-11dd-8610-bdbd52298f7b

--- flash/bwcheck/trunk/example/flowplayer.bwcheck-3.2.js
+++ flash/bwcheck/trunk/example/flowplayer.bwcheck-3.2.js
@@ -1,99 +1,101 @@
 /**
- * flowplayer.playlist
- */ 
+ * flowplayer.bwcheck
+ *
+ * Author: Daniel Rossi
+ */
 (function($) {
-	
+
 	$f.addPlugin("bwcheck", function(container, options) {
 
 
 		// self points to current Player instance
-		var self = this;	
-	
-		
+		var self = this;
+
+
 		var opts = {
 			selectedBitrateClass: 'bitrate-selected',
 			activeClass: 'bitrate-active',
 			bitrateInfoClass:'bitrate-info',
 			disabledClass: 'bitrate-disabled',
 			bwCheckPlugin: 'bwcheck',
-			template: '<a href="{bitrate}">{bitrate} k</a>',
+			template: '<a href="{bitrate}">{label}</a>',
 			disabledText: '(not valid with this player size )',
 			fadeTime: 500,
 			seperator: ""
-		};		
-		
-		$.extend(opts, options);
-		
-		var wrap = container;
-		
+		};
 
-		
+		$.extend(opts, options);
+
+		var wrap = container;
+
+
+
 		var template = null;
 		var labels = null;
 		var plugin = self.getPlugin(opts.bwCheckPlugin) || null;
-		
-		
+
+
 		var els = null;
-		
-		
+
+
 //{{{ "private" functions
-				
+
 		function parseTemplate(values) {
-			var el = template; 
-	
-			$.each(values, function(key, val) {	
+			var el = template;
+
+			$.each(values, function(key, val) {
 				if (key=="bitrate" && labels) {
 					var label = (plugin && labels.length > 0 && labels[val] ? labels[val] : val + " k");
 					if (label) {
 						el = el.replace("\{label\}", label).replace("$%7B" +key+ "%7D", label);
 					}
 				}
-				el = el.replace("\{" +key+ "\}", val).replace("$%7B" +key+ "%7D", val);			
-			}); 
-			
+				el = el.replace("\{" +key+ "\}", val).replace("$%7B" +key+ "%7D", val);
+			});
+
 			return el;
 		}
-		
+
 
 		function buildBitrateList() {
 			wrap.fadeOut(opts.fadeTime).empty();
-		
-			
+
+
 			labels = plugin.getLabels();
 			var widthCheck = (plugin.getSelectionStrategy() == "default");
 			var containerWidth = $("#" + self.id()).width();
-			
-			
+
+
 			var index = 0;
-			$.each(self.getClip().bitrates, function() { 
+			$.each(self.getClip().bitrates, function() {
 				var el = parseTemplate(this);
-				el = $(el);	
+				el = $(el);
 				el.attr("index",this.bitrate);
-		
-				
-				
+
+
+
 				if (this.width > containerWidth && widthCheck) {
-					
-					
+
+
 					if (el.is('a')) {
 						el.removeAttr("href");
 						el.addClass(opts.disabledClass);
 					}
-					
+
 					if (el.is('input')) {
 						el.attr("disabled");
 						el.addClass(opts.disabledClass);
 					}
 					el.find('a').removeAttr("href").addClass(opts.disabledClass);
 					el.find('input').attr("disabled",true).addClass(opts.disabledClass);
-			
+
 					wrap.append(el);
 					wrap.append(" " + opts.disabledText + " ");
 				} else {
 					el.addClass(opts.activeClass);
-					el.click(function() {	
+					el.click(function() {
 						el.removeClass(opts.activeClass);
-						
+
 						wrap.children().removeClass(opts.selectedBitrateClass).addClass(opts.activeClass);
 						el.addClass(opts.selectedBitrateClass);
 						play($(this).attr("index"));
@@ -101,13 +103,13 @@
 					});
 					wrap.append(el);
 				}
-				
+
 				if (index < self.getClip().bitrates.length - 1) wrap.append(opts.seperator);
 				index++;
-			});				
-			
-			
-			
+			});
+
+
+
 			//if the parent div wrapper is set to display:none fade in the parent
 			if (wrap.parent().css('display') == "none") {
 				wrap.show();
@@ -115,44 +117,44 @@
 			} else {
 				wrap.fadeIn(opts.fadeTime);
 			}
-		} 
+		}
 
 		function play(bitrate)  {
 			if (!plugin) return false;
-			
+
 			plugin.setBitrate(bitrate);
-		
+
 			return false;
-		}	
-		
-		
+		}
+
+
 		function clearCSS() {
 			els.removeClass(opts.bitrateClass);
 			els.removeClass(opts.selectedBitrateClass);
-			els.removeClass(opts.bitrateInfoClass);			
+			els.removeClass(opts.bitrateInfoClass);
 		}
-		
 
-//}}}  
-		
-		 
-		/* setup playlists with onClick handlers */ 
-		
+
+//}}}
+
+
+		/* setup playlists with onClick handlers */
+
 		// internal playlist
 		function showBitrateList() {
-	
+
 			wrap = $(wrap);
 			if (self.getClip().bitrates.length > 0) {
-				
-				template = wrap.is(":empty") ? opts.template : wrap.html(); 
-				
-				
-				
-				buildBitrateList();			
 
-			} 
+				template = wrap.is(":empty") ? opts.template : wrap.html();
+
+
+
+				buildBitrateList();
+
+			}
 		}
-		
+
 		function setupEvents() {
 			  if (plugin) {
 				 plugin.onStreamSwitch(function(mappedBitrate, streamName, oldStreamName) {
@@ -160,40 +162,37 @@
 					 //getEl(mappedBitrate.bitrate).addClass(opts.selectedBitrateClass);
 					 alert("yes");
 			 	 });
-		
+
 			 	 plugin.onBwDone(function(mappedBitrate, detectedBitrate) {
 			 		//getEl(mappedBitrate.bitrate).removeClass(opts.bitrateClass);
 					//getEl(mappedBitrate.bitrate).addClass(opts.selectedBitrateClass);
 			 		 alert("yes");
 			 		//wrap.filter("[index=" + mappedBitrate.bitrate + "]").hide();
 			 	 });
-			 
+
 			  }
 		}
-		
-		
-		// onBeforeBeginBegin
-		self.onBeforeBegin(function(clip) {
+
+
+		self.onStart(function(clip) {
 			showBitrateList();
 			setupEvents();
-		});	
-		
-		
-		
-		// onUnload
-		self.onUnload(function() {
-			//clearCSS();		
 		});
-		
-		
-		// onClipAdd
-		self.onClipAdd(function(clip, index) {	
-			//els.eq(index).before(toString(clip));			
-			//bindClicks(); 
-		});		
-		
+
+
+
+		self.onUnload(function() {
+			//clearCSS();
+		});
+
+
+		self.onClipAdd(function(clip, index) {
+			//els.eq(index).before(toString(clip));
+			//bindClicks();
+		});
+
 		return self;
-		
+
 	});
-		
-})(jQuery);		
+
+})(jQuery);
